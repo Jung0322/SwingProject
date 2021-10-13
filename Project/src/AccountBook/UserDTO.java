1fa2b8e7cd20549c9e8bde142933e4d3283c285b
ahreum <dnflsms357@gmail.com>
2021-10-13 16:02:32 +0900

    userDTO

---
+++ Project/src/AccountBook/UserDTO.java
@@ -0,0 +1,21 @@
+package AccountBook;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+
+public class UserDTO {
+	private String id;
+	private String pw;
+	private String name;
+
+}
