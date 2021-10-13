b3edcaedfefc3bb9ffa0b5fe79f1df49072444eb
Juan <cabriogt@gmail.com>
2021-10-13 16:36:20 +0900

    InfoDTO update

--- Project/src/AccountBook/InfoDTO.java
+++ Project/src/AccountBook/InfoDTO.java
@@ -1,5 +1,22 @@
 package AccountBook;
 
 public class InfoDTO {
+	
+	@Setter
+	@Getter 
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@ToString
+
+	public class EmpDTO {
+		private int no;
+		private int money;
+		private int content;
+		private int sort;
+		private Date day;
+		private int id;
+		
+	
+	
 
 }
