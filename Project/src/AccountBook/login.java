18b616117e0df2536f5995b38641b5a41e9e3b97
jung <3dnr22@gmail.com>
2021-10-14 17:47:37 +0900

    화면전환

--- Project/src/AccountBook/login.java
+++ Project/src/AccountBook/login.java
@@ -127,7 +127,8 @@ public class login extends JFrame {
 					id = textId.getText();
 					Main main = new Main();
 					main.setVisible(true);
-					frame.setVisible(false);
+//					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					dispose();
 				}else {
 					JOptionPane.showMessageDialog(null, "로그인이 실패히었습니다.");
 				}
@@ -143,7 +144,8 @@ public class login extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				Join join = new Join();
 				join.setVisible(true);
-				frame.setVisible(false);
+//				frame.setVisible(false);
+				dispose();
 			}
 		});
 		panel_2.add(btnJoin);
