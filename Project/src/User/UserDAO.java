481a6a03f8ddda4e45ba9ac3e6f0abe065a72d90
Juan <cabriogt@gmail.com>
2021-10-14 16:55:27 +0900

    update

--- Project/src/User/UserDAO.java
+++ Project/src/User/UserDAO.java
@@ -2,6 +2,12 @@ package User;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class UserDAO {
 	static {
@@ -24,4 +30,39 @@ public class UserDAO {
 		}
 		return con;
 	}
+	
+	public List<UserDTO> select() {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		List<UserDTO> list = new ArrayList<UserDTO>();
+		
+		try {
+			con = getConnection();
+			String sql = "select * from membertbl";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				UserDTO dto = new UserDTO();
+				dto.setId(rs.getString("id"));
+				dto.setPw(rs.getString("pw"));
+				dto.setName(rs.getString("name"));
+				
+				list.add(dto);
+			}			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				con.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return list;
+	}
+
+	
 }
